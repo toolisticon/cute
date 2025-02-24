0e289e3985341e1b231d59288a36db561e4d7c5b
Tobias Stamann <tobias@stamann.dev>
2025-02-24 02:35:25 +0100

    Fix build

---
+++ extension/plainjava/src/main/java/io/toolisticon/cute/extension/plainjava/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains all public classes and interfaces intentioned to be used as part of the public api.
+ */
+@io.toolisticon.spiap.api.SpiServiceLocator(io.toolisticon.cute.extension.api.AssertionSpi.class)
+package io.toolisticon.cute.extension.plainjava;
